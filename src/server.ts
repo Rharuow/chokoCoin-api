fea993d57efde8fb2522b5a42a1efca1ab841ece
Harysson <haryssonsoares@gmail.com>
2021-12-02 17:13:59 -0300

    chore: cors config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 import "./database";
 require("dotenv").config();
+import formData from "express-form-data";
 
 const app = express();
 const port = process.env.HOST_PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(formData.format());
+app.use(formData.stream());
+app.use(formData.union());
+app.use(cors());
 
 app.use(router);
 
