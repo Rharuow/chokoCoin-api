260105a27b72e5cec38b91f9e807d0a39223ce2a
Harysson <haryssonsoares@gmail.com>
2021-12-02 17:10:29 -0300

    feat: routes to project

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,23 @@ import { RootController } from "./controllers/RootController";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { DeleteUserController } from "./controllers/User/DeleteUserController";
 import { ListUserController } from "./controllers/User/ListUserController";
+import { CreateProjectController } from "./controllers/Project/CreateProjectController";
+import { ListProjectController } from "./controllers/Project/ListProjectController";
 import { CreateSessionController } from "./controllers/Session/CreateSessionController";
+
 import { ensurePermissionRegister } from "./middlewares/ensurePermissionRegister";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const listUserController = new ListUserController();
+
 const createSessionController = new CreateSessionController();
+
+const createProjectController = new CreateProjectController();
+const listProjectController = new ListProjectController();
 
 const rootController = new RootController();
 
@@ -22,6 +30,8 @@ router.post(
   "/login",
   /*ensurePermissionRegister,*/ createSessionController.handle
 );
+router.post("/project", ensureAuthenticated, createProjectController.handle);
+router.get("/project", ensureAuthenticated, listProjectController.handle);
 
 router.get("/", rootController.run);
 
