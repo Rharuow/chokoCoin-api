84d4f85dc7d55594e7f29e5a0aad1d5ec3b6fff5
Harysson <haryssonsoares@gmail.com>
2021-12-21 09:42:14 -0300

    refact partner's project serialized

--- src/repositories/UserToProjectRepository.ts
+++ src/repositories/UserToProjectRepository.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
 import { UserToProject } from "../entities/UserToProject";
 import { ProjectRepository } from "./ProjectRepository";
@@ -5,26 +6,29 @@ import { UserRepository } from "./UserRepository";
 
 @EntityRepository(UserToProject)
 export class UserToProjectRepository extends Repository<UserToProject> {
-    async findProjectsByPartners() {
+  async findProjectsByPartners() {
+    const projectsRepository = getCustomRepository(ProjectRepository);
 
-        const projectsRepository = getCustomRepository(ProjectRepository)
+    const partnersByProject = await this.find({
+      relations: ["partners", "projects"],
+    });
 
-        const partnersByProject = await this.find({
-            relations: ["partners", 'projects'],
-          });
+    const allProjects = await projectsRepository.find();
 
-        const allProjects = await projectsRepository.find()
-
-        const projects = allProjects.map((project) => ({
-                id: project.id,
-                name: project.name,
-                value: project.value,
-                partners: partnersByProject
-                .filter((partnerByProject) => partnerByProject.project_id === project.id)
-                .map(({partners, value, ...rest}) => ({...partners, value}))
-            })
+    const projects = allProjects.map((project) => ({
+      id: project.id,
+      name: project.name,
+      value: project.value,
+      partners: partnersByProject
+        .filter(
+          (partnerByProject) => partnerByProject.project_id === project.id
         )
+        .map(({ partners, value }) => {
+          _.unset(partners, ["password", "token", "created_at", "updated_at"]);
+          return { ...partners, value };
+        }),
+    }));
 
-        return projects
-    }
+    return projects;
+  }
 }
